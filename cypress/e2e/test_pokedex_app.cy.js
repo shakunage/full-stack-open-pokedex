180d616d8a97859974566e2aa3e28a3c212e1f6b
shakunage <veikka.junttila1@gmail.com>
2024-08-12 17:36:05 +0900

    add another cypress test

--- cypress/e2e/test_pokedex_app.cy.js
+++ cypress/e2e/test_pokedex_app.cy.js
@@ -5,4 +5,9 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href="/pokemon/ivysaur"]').click()
+    cy.contains('chlorophyll')
+  })
 })
